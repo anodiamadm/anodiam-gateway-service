a51eb417abf5a45680946ca6270450199f7a3f0e
subrata-hazra <subratah@gmail.com>
2022-10-11 17:51:05 +0530

    JWT signature verification added

--- src/main/java/com/anodiam/gateway/config/AnodiamAuthenticationFilter.java
+++ src/main/java/com/anodiam/gateway/config/AnodiamAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class AnodiamAuthenticationFilter implements WebFilter {
                     JwtDecoder jwtDecoder = new AnodiamJwtDecoder(jwtSecret);
                     Jwt jwt = jwtDecoder.decode(token);
                     Authentication authentication = new AnodiamAuthentication(jwt);
-                    return chain.filter(exchange).subscriberContext(ReactiveSecurityContextHolder.withAuthentication(authentication));
+                    return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
                 }
             }
         } catch (Exception e) {
